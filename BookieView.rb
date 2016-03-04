066c875d9f33d1a0fa449ef9514cf4499d77798f
xavierlei <xavier.c.f@hotmail.com>
2016-03-04 16:48:29 +0000

    feature bookie

---
+++ BookieView.rb
@@ -0,0 +1,17 @@
+class BookieView < Object
+	def printView(username,password)
+		puts "bookie name: #{username}"
+		puts "password #{password}"
+	end
+	def createView
+		puts "type bookie username"
+		usr = gets.chomp
+		puts "type your password"
+		passwd = gets.chomp
+		data = [usr,passwd]
+	end
+	def updateView
+		puts "type your new password"
+		password = gets.chomp
+	end
+end
---
+++ testBookie.rb
@@ -0,0 +1,6 @@
+require_relative 'BookieController.rb'
+bookie = BookieController.new
+bookie.create
+bookie.updateView
+bookie.update
+bookie.updateView
